27c0b57c63af674e84b82ce6ba116a1395e233b4
Sai Manjula Simhadri <manjula.simhadri@ttu.edu>
2015-10-04 16:23:37 -0500

    Initial Code Push

---
+++ DFDLProject/src/main/java/svv/project/LaunchApplication.java
@@ -0,0 +1,13 @@
+package svv.project;
+
+public class LaunchApplication 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		SchemaValidator sv = new SchemaValidator();
+		
+		sv.Execute();
+	}
+
+}
